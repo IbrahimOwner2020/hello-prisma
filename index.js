64e63bce18f1c2aa17977a02fefbb17f817d644b
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-08-01 16:06:11 +0300

    fix: apis

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const prisma = new PrismaClient();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/posts", async (req, res) => {
 	const posts = await prisma.post.findMany();
 
@@ -23,16 +25,57 @@ app.get("/posts/:id", async (req, res) => {
 	res.json(post);
 });
 
-// GETTING UNDEFINED FROM REQ.BODY
-// app.post("/create/post", async (req, res) => {
-// 	console.log(req.body);
-// 	// const data = req.body;
-// 	// console.log(data);
-// 	// const post = await prisma.post.create({
-// 	// 	data: data,
-// 	// });
+app.post("/create/post", async (req, res) => {
+	const data = req.body;
+	console.log(data);
+	const post = await prisma.post.create({
+		data: data,
+	});
 
-// 	res.json("Send completed");
-// });
+	res.json({
+		message: "Post created",
+		data: post,
+	});
+});
+
+app.get("/users", async (req, res) => {
+	await prisma.user
+		.findMany()
+		.then((users) => {
+			res.json({
+				message: "Users fetched successfully",
+				data: users,
+			});
+		})
+		.catch((error) => {
+			res.json(400, {
+				message: "Error fetching users",
+				data: error,
+			});
+		});
+});
+
+app.get("/users/:id", async (req, res) => {
+	const { id } = req.params;
+
+	await prisma.user
+		.findUnique({
+			where: {
+				id,
+			},
+		})
+		.then((user) => {
+			res.json({
+				message: "User fetched successfully",
+				data: user,
+			});
+		})
+		.catch((error) => {
+			res.json(500, {
+				message: "Error fetching user",
+				error: error,
+			});
+		});
+});
 
 app.listen(4000, () => console.log("Server is running on port 4000"));
